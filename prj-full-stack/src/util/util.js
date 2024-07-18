4403ee315a36f3d582e0dc9e1e2c5e9ea94382d4
Hung-DevOps <111832057+HungNQ-DevOps@users.noreply.github.com>
2024-07-18 10:16:36 +0900

    Add files via upload

--- prj-full-stack/src/util/util.js
+++ prj-full-stack/src/util/util.js
@@ -12,8 +12,8 @@ import Jimp from "jimp";
  export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
+
       const photo = await Jimp.read(inputURL);
-      console.log(inputURL);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
@@ -23,6 +23,7 @@ import Jimp from "jimp";
         .write(outpath, (img) => {
           resolve(outpath);
         });
+
     } catch (error) {
       reject(error);
     }

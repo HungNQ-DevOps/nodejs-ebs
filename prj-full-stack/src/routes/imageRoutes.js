67968dbd488c4c9f0058f177f82e4b86da07fb1f
Hung-DevOps <111832057+HungNQ-DevOps@users.noreply.github.com>
2024-07-18 10:17:13 +0900

    Add files via upload

--- prj-full-stack/src/routes/imageRoutes.js
+++ prj-full-stack/src/routes/imageRoutes.js
@@ -1,20 +1,42 @@
 import express from "express";
 export const router = express.Router();
+import fetch from "node-fetch";
+import FileType from "file-type";
 import {filterImageFromURL, deleteLocalFiles} from '../util/util.js';
+import { StatusCodes, getReasonPhrase } from "http-status-codes";
 
 router.get( "/", async (req, res) => {
 
-    const image_url = req.query.image_url.toString();
+    const image_url = req.query.image_url;
 
     if (!image_url) {
-      res.status(404).send("Image URL is invalid");
+      res.status(StatusCodes.BAD_REQUEST).send({
+        error: getReasonPhrase(StatusCodes.BAD_REQUEST),
+        message: "The image URL is required"
+      });
 
     } else {
-        const filterImage = await filterImageFromURL(image_url);
-        res.status(200).sendFile(filterImage);
-        res.on('finish', function () {
-          deleteLocalFiles[filterImage];
-        });
+
+        const resURL = await fetch(image_url);
+        const arrBuff = await resURL.arrayBuffer();
+        const buffer = Buffer.from(arrBuff);
+        const checkType = await FileType.fromBuffer(buffer);    
+        
+        if (!checkType || checkType.ext!="jpg") {
+          res.status(StatusCodes.BAD_REQUEST).send({
+            error: getReasonPhrase(StatusCodes.BAD_REQUEST),
+            message: "The image type JPG is required"
+          });
+        } else {
+
+          const filterImage = await filterImageFromURL(image_url);
+          res.status(200).sendFile(filterImage);
+          res.on('finish', function () {
+            deleteLocalFiles[filterImage];
+          });
+
+        }
+
       }
     
   } );
